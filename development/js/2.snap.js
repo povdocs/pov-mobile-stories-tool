15bd18efcef4b823738ec346ffa1951f2502b972
Sunil <admin@sunil523.com>
2016-08-22 13:03:25 -0400

    added arrow when there is content in bottom snap.

--- development/js/2.snap.js
+++ development/js/2.snap.js
@@ -39,6 +39,8 @@ app.snap = {
         content.append('<video class="snap-content-video" preload="metadata" controls><source src="'+snap.content.src+'" type="video/mp4"></video>');
       }
       content.addClass(snap.content.type);
+      section.addClass('content-more');
+      section.addClass(snap.content.type);
       content.appendTo(section);
     }
     section.appendTo('main');
